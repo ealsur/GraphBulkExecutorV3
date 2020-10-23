b407791e0b32ce6c8a8ddf425e3805b7e695ab00
Matias Quaranta <maquaran@microsoft.com>
2020-10-23 16:32:59 -0700

    Add project files.

---
+++ GraphBulkExecutorV3/Bulk/TaskExtensions.cs
@@ -0,0 +1,54 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace GraphBulkExecutorV3
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos;
+
+    internal static class TaskExtensions
+    {
+        /// <summary>
+        /// Captures the result of a Cosmos item operation into an <see cref="OperationResponse{T}"/>
+        /// </summary>
+        public static Task<OperationResponse<T>> CaptureOperationResponse<T>(this Task<ItemResponse<T>> task, T item)
+        {
+            return task.ContinueWith(itemResponse =>
+            {
+                if (itemResponse.Status == TaskStatus.RanToCompletion 
+                    && !itemResponse.IsFaulted 
+                    && !itemResponse.IsCanceled)
+                {
+                    return new OperationResponse<T>()
+                    {
+                        Item = item,
+                        IsSuccessful = true,
+                        RequestUnitsConsumed = task.Result.RequestCharge
+                    };
+                }
+
+                AggregateException innerExceptions = itemResponse.Exception.Flatten();
+                if (innerExceptions.InnerExceptions.FirstOrDefault(innerEx => innerEx is CosmosException) is CosmosException cosmosException)
+                {
+                    return new OperationResponse<T>()
+                    {
+                        Item = item,
+                        RequestUnitsConsumed = cosmosException.RequestCharge,
+                        IsSuccessful = false,
+                        CosmosException = cosmosException
+                    };
+                }
+
+                return new OperationResponse<T>()
+                {
+                    Item = item,
+                    IsSuccessful = false,
+                    CosmosException = innerExceptions.InnerExceptions.FirstOrDefault()
+                };
+            });
+        }
+    }
+}
